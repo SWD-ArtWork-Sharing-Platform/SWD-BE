65f07fde8135583181ffd1ea3463c87a076116aa
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-03-18 11:04:12 +0700

    commit

--- Management/Program.cs
+++ Management/Program.cs
@@ -101,17 +101,17 @@ namespace Management
 
                 options.AddPolicy("ARTWORKMANAGEMENT", policy =>
                 {
-                    policy.RequireRole(SD.ADMIN,SD.MODERATOR,SD.CREATOR);
+                    policy.RequireRole(SD.ADMIN, SD.MODERATOR, SD.CREATOR);
                 });
 
                 options.AddPolicy("ORGANIZATION", policy =>
                 {
-                    policy.RequireRole(SD.ADMIN,SD.MODERATOR);
+                    policy.RequireRole(SD.ADMIN, SD.MODERATOR);
                 });
 
                 options.AddPolicy("CUSTOMER_USER", policy =>
                 {
-                    policy.RequireRole(SD.CUSTOMER,SD.CREATOR);
+                    policy.RequireRole(SD.CUSTOMER, SD.CREATOR);
                 });
             });
 
